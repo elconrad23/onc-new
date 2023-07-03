60b3a3962ff150dc99a687b63f53ca13c7fdd208
Conrad Waako K <eleazarconrad@gmail.com>
2023-07-03 09:48:36 +0200

    with js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-//const smelte=require("smelte/rollup-plugin-smelte");
 
 export default defineConfig({
 	plugins: [sveltekit(),]
